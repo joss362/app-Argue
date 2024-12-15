52bb6dcc6aa1890171962f5b3e2b537111bb2f3e
joss362 <velasquej88@gmail.com>
2024-12-15 18:12:01 -0500

    cambio

--- src/app/auth/features/sing-in/sing-in.component.ts
+++ src/app/auth/features/sing-in/sing-in.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sing-in',
+  standalone:true,
   imports: [],
   templateUrl: './sing-in.component.html',
-  styleUrl: './sing-in.component.css',
+  styles: ``
 })
-export default class SingInComponent {}
+export default class SingInComponent {
+
+}
--- src/app/auth/features/sing-up/sing-up.component.ts
+++ src/app/auth/features/sing-up/sing-up.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sing-up',
+  standalone:true,
   imports: [],
   templateUrl: './sing-up.component.html',
-  styleUrl: './sing-up.component.css'
+  styles: ``
 })
 export default class SingUpComponent {
 
