a1152f3225f19884bb26ca34a1c85feae594572b
joss362 <velasquej88@gmail.com>
2024-12-15 17:47:12 -0500

    ruteo y sing-in

---
+++ src/app/auth/features/auth.routers.ts
@@ -0,0 +1,12 @@
+import { Route, Routes } from "@angular/router";
+
+export default [
+    {
+        path: 'sing-in',
+        loadComponent: () => import('./sing-in/sing-in.component')
+    },
+    {
+        path: 'sing-up',
+        loadComponent: () => import('./sing-up/sing-up.component')
+    }
+] as Routes
---
+++ src/app/auth/features/sing-in/sing-in.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sing-in',
+  imports: [],
+  templateUrl: './sing-in.component.html',
+  styleUrl: './sing-in.component.css',
+})
+export default class SingInComponent {}
