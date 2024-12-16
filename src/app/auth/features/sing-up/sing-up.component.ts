c9ac20e38791a98a3fa6cea23d65b889ea350237
joss362 <velasquej88@gmail.com>
2024-12-15 19:08:42 -0500

    test

--- src/app/auth/features/sing-up/sing-up.component.ts
+++ src/app/auth/features/sing-up/sing-up.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {FormBuilder, FormControl, ReactiveFormsModule, Validators} from '@angular/forms'
 
+interface FormSingUp{
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+}
 @Component({
   selector: 'app-sing-up',
   standalone:true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sing-up.component.html',
   styles: ``
 })
 export default class SingUpComponent {
+  private _formBuilder =inject(FormBuilder);
+
+  form = this._formBuilder.group<FormSingUp>({
+    email: this._formBuilder.control('', [Validators.required,Validators.email]),
+    password:this._formBuilder.control('', Validators.required),
+  })
+
+  submit(){
+    if(this.form.invalid) return;
+
+    const {email,password}=this.form.value;
+
+    if(!email || !password) return;
+
+    console.log({email,password})
+  }
 
 }
