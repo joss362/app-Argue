dfde32d747e95554ff9422e1d1d9bf5ba5b396e1
joss362 <velasquej88@gmail.com>
2024-12-17 22:41:03 -0500

    perfil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,7 +12,7 @@ export const routes: Routes = [
     },
     {
         path:'**',
-        redirectTo: '/task',
+        redirectTo: '/Sing-in',
         
     }
     
--- src/app/task/features/task-list/task-list.component.ts
+++ src/app/task/features/task-list/task-list.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './task-list.component.html',
   styles: ``
 })
